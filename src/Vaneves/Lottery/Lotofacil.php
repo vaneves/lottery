b0afc13d9f801cf93657fa1e338faa8b6007d43e
Van Neves <vaneves@vaneves.com>
2016-02-24 23:13:28 -0300

    limitação da listagem dos rateios

--- src/Vaneves/Lottery/Lotofacil.php
+++ src/Vaneves/Lottery/Lotofacil.php
@@ -8,4 +8,11 @@ class Lotofacil extends Lottery
 	{
 		return $this->queryValues('//*[@id="resultados"]/div[2]/div/div/table/tbody/tr/td');
 	}
+
+	public function listAwards()
+	{
+		$awards = parent::listAwards();
+		$awards[0]->apportionments = array_slice($awards[0]->apportionments, 0, 5);
+		return $awards;
+	}
 }
--- src/Vaneves/Lottery/Lotomania.php
+++ src/Vaneves/Lottery/Lotomania.php
@@ -8,4 +8,11 @@ class Lotomania extends Lottery
 	{
 		return $this->queryValues('//table[contains(@class, "lotomania")]/*/td');
 	}
+
+	public function listAwards()
+	{
+		$awards = parent::listAwards();
+		$awards[0]->apportionments = array_slice($awards[0]->apportionments, 0, 6);
+		return $awards;
+	}
 }
